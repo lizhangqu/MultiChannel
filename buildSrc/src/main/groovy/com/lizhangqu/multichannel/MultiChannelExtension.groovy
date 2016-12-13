fb1f7fd859bee1fd0de0908fdb4f4de92722762a
李樟取 <lizhangqu@weidian.com>
2016-12-13 13:43:42 +0800

    remove

--- buildSrc/src/main/groovy/com/lizhangqu/multichannel/MultiChannelExtension.groovy
+++ buildSrc/src/main/groovy/com/lizhangqu/multichannel/MultiChannelExtension.groovy
@@ -5,8 +5,6 @@ import org.gradle.api.Project
 class MultiChannelExtension {
     public static final PLUGIN_EXTENTION = "multiChannel"
     HashSet<String> channels = []
-    boolean resign = true
-
     MultiChannelExtension(Project project) {
 
     }
